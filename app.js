8bf5edfb9ee877990e5b62c97b13d23239d17a47
Andrea Reginato <andrea.reginato@gmail.com>
2013-06-20 08:39:06 +0200

    Working socket.io plus loop on notifying correct apps

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express  = require('express')
+var mongoose = require('mongoose')
+  , express  = require('express')
   , app      = express()
   , server   = require('http').createServer(app)
-  , io       = require('socket.io').listen(server)
-  , mongoose = require('mongoose');
+  , io       = require('socket.io').listen(server);
 
-var loop         = require('./app/lib/loop.js')
-  , Event        = require('./app/models/jobs/event')
+var Event        = require('./app/models/jobs/event')
   , User         = require('./app/models/people/user')
   , Application  = require('./app/models/people/application')
   , AccessToken  = require('./app/models/people/access_token');
+
+var _  = require('underscore');
 
 
 
@@ -16,28 +17,27 @@ var loop         = require('./app/lib/loop.js')
  * Socket.io server *
  * ---------------- */
 
-server.listen(process.env.PORT);
-
-// TODO move to a test section
 app.configure(function() {
   app.use(express.static(__dirname + '/app/assets/javascripts'));
   app.use(express.static(__dirname + '/app/assets'));
 });
 
-// TODO move to a test section
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-// TODO update to /test
+// TODO better test logics
 app.put('/update', function (request, response) {
   io.sockets.emit('devices:update', { id: '1' });
   response.json({});
 });
 
+// TODO better connection logics
 io.sockets.on('connection', function (socket) {
   socket.emit('devices:update', { id: '0' });
 });
+
+server.listen(process.env.PORT);
 
 
 
@@ -45,4 +45,27 @@ io.sockets.on('connection', function (socket) {
  * Realtime loop *
  * ------------- */
 
-loop.execute();
+// Find the valid tokens associated to each event
+Event.find({ realtime_processed: false }).tailable().stream()
+  .on('data', function(collection) { findTokens(collection) });
+
+// Returns all valid access tokens and notifies the apps using them
+var findTokens = function(event) {
+
+  // Find all acces tokens to notify
+  var tokens = function() {
+    event.findAccessTokens(stream);
+  }
+
+  // Send the notification to the authorized clients
+  var stream = function(err, tokens) {
+    _.each(tokens, function(token) {
+      io.sockets.emit('devices:update', { id: token.token, type: 'token loop' });
+    });
+    event.realtime_processed = true;
+    event.save();
+  }
+
+  tokens();
+};
+
--- app/lib/loop.js
+++ app/lib/loop.js
@@ -1,10 +1,45 @@
-var mongoose     = require('mongoose')
-  , Event        = require('../models/jobs/event')
+var express  = require('express')
+  , app      = express()
+  , server   = require('http').createServer(app)
+  , io       = require('socket.io').listen(server)
+  , mongoose = require('mongoose');
+
+var Event        = require('../models/jobs/event')
   , User         = require('../models/people/user')
   , Application  = require('../models/people/application')
   , AccessToken  = require('../models/people/access_token');
 
-var _ = require('underscore');
+var _  = require('underscore');
+
+
+
+/* ---------------- *
+ * Socket.io server *
+ * ---------------- */
+
+server.listen(process.env.PORT);
+
+// TODO move to a test section
+app.configure(function() {
+  app.use(express.static(__dirname + '/../app/assets/javascripts'));
+  app.use(express.static(__dirname + '/../app/assets'));
+});
+
+// TODO move to a test section
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// TODO update to /test
+app.put('/update', function (request, response) {
+  io.sockets.emit('devices:update', { id: '1' });
+  response.json({});
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('devices:update', { id: '0' });
+});
+
 
 
 /* ------------- *
@@ -30,10 +65,11 @@ var findTokens = function(event) {
   // Send the notification to the authorized clients
   var stream = function(err, tokens) {
     tokens = _.map(tokens, function(token){ return token.token });
-    if (process.env.DEBUG) { console.log('TOKENS:', tokens) }
+    io.sockets.emit('devices:update', { id: token, type: 'token loop' });
     event.realtime_processed = true;
     event.save();
   }
 
+  // Everything starts here
   tokens();
 };
