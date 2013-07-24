5312fc2cd93dc1cc44ebeab0a616fa6df4ad6c55
Andrea Reginato <andrea.reginato@gmail.com>
2013-07-24 18:05:52 +0200

    Improved inline documentation

--- app.js
+++ app.js
@@ -6,26 +6,33 @@ var mongoose = require('mongoose')
   , debug    = require('debug')('lelylan');
 
 
-// Websocket and express app
+// *************
+// Express app
 
 app.configure(function() {
   app.use(express.static(__dirname + '/app/assets/javascripts'))
   app.use(express.static(__dirname + '/app/assets'))
 });
 
+// index test page
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html')
 });
 
+// websocket fire test page
 app.put('/test', function(request, response) {
   io.sockets.in('token-1').emit('update', { data: require('./test/fixtures/device.json') });
   response.json({});
 });
 
+
+// ***************
+// Websockets app
+
 io.sockets.on('connection', function(socket) {
   socket.on('subscribe', function(room) {
     debug('Subscribing room', room);
-    socket.join(room);
+    socket.join(room); // a room is equivalent to an access token
   });
 
   socket.emit('connected');
@@ -42,14 +49,17 @@ server.listen(process.env.NODE_PORT, function() {
     });
 });
 
-
 server.listen(process.env.NODE_PORT);
 
 
+// ***************
 // Realtime loop
 
 var _loop = require('./lib/loop');
 _loop.execute(io);
 
+
+// ***************
+// App interface
 
 module.export = { io: io, app: app };
