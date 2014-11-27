c957886073f39078c2bf4d36706cc6249b8d6252
Andrea Reginato <andrea.reginato@gmail.com>
2014-11-27 14:16:38 +0100

    Added cross domain support

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ var mongoose = require('mongoose')
 app.configure(function() {
   app.use(express.static(__dirname + '/app/assets/javascripts'))
   app.use(express.static(__dirname + '/app/assets'))
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    return next();
+  });
 });
+
 
 // index test page
 app.get('/', function(req, res) {
